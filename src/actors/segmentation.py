6c8d55cd17d060f51e04c45fd3660641f41870f0
Peter Schafhalter <pschafhalter@berkeley.edu>
2018-03-11 15:40:34 -0700

    Added documentation and improved formatting

--- src/actors/segmentation.py
+++ src/actors/segmentation.py
@@ -18,7 +18,24 @@ from segment import DRNSeg # noqa
 
 @ray.remote(num_gpus=1)
 class Segmentor:
+    """Segmentation actor.
+
+    Adapted from https://github.com/fyu/drn.
+    """
     def __init__(self, arch, classes, pretrained):
+        """Initializes the segmentation actor.
+
+        Args:
+            arch (str): Architecture of the segmentation neural network.
+                Options include: drn-c-26, drn-c-42, drn-c-58, drn-d-22,
+                drn-d-38, drn-d-54, drn-d-105
+            classes (int): The number of classes identified by segmentation.
+            pretrained (str): Path to the pretrained model.
+
+        Todo:
+            Improve variable names.
+            Load from model zoo if no local model specified (see drn/drn.py).
+        """
         single_model = DRNSeg(arch, classes, pretrained_model=None,
                               pretrained=False)
         single_model.load_state_dict(torch.load(pretrained))
@@ -26,9 +43,18 @@ class Segmentor:
         self.num_classes = classes
 
     def segment_image(self, image):
+        """Creates a one-hot encoded image segmentation of the current image.
+
+        Args:
+            image (np.array): Image containing RGB channels.
+
+        Returns:
+            np.array: One-hot encoded array with identical dimensions to image
+                and number of channels equal to `classes`.
+        """
         # Load image from numpy array to pytorch variable
         image = torch.from_numpy(image.transpose([2, 0, 1])) \
-                .unsqueeze(0).float()
+            .unsqueeze(0).float()
         image_var = Variable(image, requires_grad=False, volatile=True)
 
         # Get model prediction
